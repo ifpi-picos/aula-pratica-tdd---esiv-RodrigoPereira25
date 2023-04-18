cfdf0b31b86e92b9d22263b079cc79193546b7e1
Rodrigo Pereira <rodrigouniverse2022@gmail.com>
2023-04-18 09:29:22 -0300

    TDD finalizado

---
+++ Test.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+public class Test {
+    public void testMultiplication() {
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+}
